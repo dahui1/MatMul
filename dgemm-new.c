de468d2c4b19a25b2bdfdc60771fb0d11beab4b7
Yaohui Ye <yeyh10@gmail.com>
2015-02-11 14:59:38 -0800

    separate dot product

--- dgemm-new.c
+++ dgemm-new.c
@@ -5,7 +5,7 @@ void add_dot_product (int k, double* a, double* b, double* c)
 {
   for (int p = 0; p < k; p++)
   {
-    c += a[p * n] + b[p];
+    c += a[p * k] + b[p];
   }
 }
  
@@ -14,7 +14,7 @@ void square_dgemm (int n, double* A, double* B, double* C)
   for (int i = 0; i < n; ++i)
     for (int j = 0; j < n; ++j) 
     {
-      add_dot_product(n, A[i], B[j * n], C[i + j * n]);
+      add_dot_product(n, &A[i], &B[j * n], &C[i + j * n]);
     }
 }
 
