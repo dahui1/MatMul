b356f4c05a6669ef840103621d1d342ad93a7891
Julie Newcomb <jn80842@gmail.com>
2015-02-12 10:59:11 -0800

    tutorial page 6

--- dgemm-new.c
+++ dgemm-new.c
@@ -11,37 +11,29 @@ void add_dot(int lda, int stride, double* A, double* B, double* C)
   }
 }
 
-/*
-  AddDot( k, &A( 0, 0 ), lda, &B( 0, 0 ), &C( 0, 0 ) );
-  AddDot( k, &A( 0, 0 ), lda, &B( 0, 1 ), &C( 0, 1 ) );
-  AddDot( k, &A( 0, 0 ), lda, &B( 0, 2 ), &C( 0, 2 ) );
-  AddDot( k, &A( 0, 0 ), lda, &B( 0, 3 ), &C( 0, 3 ) );
-  */
 
 
 // calculate 4 values of C, lying on a row
 void add_dot_1x4(int lda, int stride, double* A, double* B, double* C) 
 {
-  //add_dot(lda,stride,A,B,C);
+  register double A_row;
+  register double c0, c1, c2, c3;
+  c0 = 0.0;
+  c1 = 0.0;
+  c2 = 0.0;
+  c3 = 0.0;
   for (int k=0; k<lda;k++)
   {
-    C[0] += A[k*stride] * B[k];
+    A_row = A[k*stride];
+    c0 += A_row * B[k];
+    c1 += A_row * B[lda+k];
+    c2 += A_row * B[k+2*lda];
+    c3 += A_row * B[k+3*lda];
   }
-  //add_dot(lda,stride,A,B+lda,C+lda);
-  for (int k=0; k<lda;k++)
-  {
-    C[lda] += A[k*stride] * B[lda+k];
-  }
-  //add_dot(lda,stride,A,B+2*lda,C+2*lda);
-  for (int k=0; k<lda;k++)
-  {
-    C[2*lda] += A[k*stride] * B[k+2*lda];
-  }
-  //add_dot(lda,stride,A,B+3*lda,C+3*lda;
-  for (int k=0; k<lda;k++)
-  {
-    C[3*lda] += A[k*stride] * B[k+3*lda];
-  }
+  C[0] += c0;
+  C[lda] += c1;
+  C[2*lda] += c2;
+  C[3*lda] += c3;
 }
 
 /* This routine performs a dgemm operation
@@ -56,7 +48,6 @@ void square_dgemm (int lda, double* A, double* B, double* C)
     /* For each row i of A */
     for (int i = 0; i < lda; i ++) // rows of C
     {
-      //add_dot(lda,lda,A+i,B+j*lda,C+i+j*lda);
       add_dot_1x4(lda,lda,A+i,B+j*lda,C+i+j*lda);
     }
   }
