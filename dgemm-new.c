afbfa60112e390e9a28b7674ffd99851cf90bbb5
Yaohui Ye <yeyh10@gmail.com>
2015-02-11 22:06:08 -0800

    in progress

--- dgemm-new.c
+++ dgemm-new.c
@@ -1,5 +1,7 @@
 const char* dgemm_desc = "New dgemm.";
 
+#include <stdlib.h>
+#include <string.h>
 
 void add_dot_products14 (int k, double* a, double* b, double* c)
 {
@@ -8,10 +10,10 @@ void add_dot_products14 (int k, double* a, double* b, double* c)
   double c2 = 0;
   double c3 = 0;
  
-  double* bp0 = &b[0 + 0 * k];
-  double* bp1 = &b[0 + 1 * k];
-  double* bp2 = &b[0 + 2 * k];
-  double* bp3 = &b[0 + 3 * k];
+  double* bp0 = &b[0];
+  double* bp1 = &b[k];
+  double* bp2 = &b[2 * k];
+  double* bp3 = &b[3 * k];
  
   for (int p = 0; p < k; p += 4)
   {
@@ -53,10 +55,31 @@ void add_dot_products14 (int k, double* a, double* b, double* c)
  
 void square_dgemm (int n, double* A, double* B, double* C)
 {
-  for (int j = 0; j < n; j += 4)
-    for (int i = 0; i < n; i++) 
+  int n4 = n;
+  if (n4 % 4 != 0) {
+    n4 = n - n4 % 4 + 4;
+  }
+
+  double* buf = (double*) calloc (3 * n4 * n4, sizeof(double));
+
+  double* newA = buf + 0;
+  double* newB = newA + n4 * n4;
+  double* newC = newB + n4 * n4;  
+
+  for(int i = 0; i < n; i++){
+    memcpy(newA + i * n4, A + i * n, n * sizeof(double));
+    memcpy(newB + i * n4, B + i * n, n * sizeof(double));
+    memcpy(newC + i * n4, C + i * n, n * sizeof(double));
+  }
+
+  for (int j = 0; j < n4; j += 4)
+    for (int i = 0; i < n4; i++) 
     {
-      add_dot_products14(n, &A[i], &B[j * n], &C[i + j * n]);
+      add_dot_products14(n4, &newA[i], &newB[j * n4], &newC[i + j * n4]);
     }
+
+  for (int i = 0; i < n; i++) {
+    memcpy(C + i * n, newC + i * n4, n * sizeof(double));
+  }
 }
 
