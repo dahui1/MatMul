fddd9dcd308517f1c9950a94d708b9410c6b8d57
Julie Newcomb <jn80842@gmail.com>
2015-02-11 17:34:33 -0800

    my versions (in the right branch)

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -1,3 +1,5 @@
+#include <emmintrin.h>
+
 const char* dgemm_desc = "Simple blocked dgemm.";
 
 #if !defined(BLOCK_SIZE)
@@ -9,41 +11,75 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 /* This auxiliary subroutine performs a smaller dgemm operation
  *  C := C + A * B
  * where C is M-by-N, A is M-by-K, and B is K-by-N. */
-static void do_block (int lda, int M, int N, int K, double* A, double* B, double* C)
+static void do_block (int lda, int M, int N, int K, double* A, double* B, double *restrict C)
 {
-  /* For each row i of A */
-  for (int j = 0; j < N; ++j)
-    /* For each column j of B */ 
-    for (int k = 0; k < K; ++k) 
+  /*
+   * copy optimization on this subblock?
+   * try just for A?
+  */
+
+
+  /* For each column j of B */ 
+  for (int j = 0; j < N; ++j) 
     {
-      /* Compute C(i,j) */
-      double bkj = B[k+j*lda];
-      //double cij = C[i+j*lda];
-      for (int i = 0; i < M; ++i)
-	C[i+j*lda] += A[i+k*lda] * bkj;
-      //C[i+j*lda] = cij;
+    for (int k = 0; k < K; ++k)
+    {
+      __m128d bvec = _mm_load1_pd(&B[k+j*lda]);
+    /* For each row i of A */
+    for (int i = 0; i < M/8*8; i+=8)
+      {
+        /* Compute C(i,j) */
+        __m128d cvec = _mm_loadu_pd(&C[i+j*lda]);
+        __m128d avec = _mm_loadu_pd(&A[i+k*lda]);
+        __m128d cvec1 = _mm_loadu_pd(&C[i+j*lda+2]);
+        __m128d avec1 = _mm_loadu_pd(&A[i+k*lda+2]);
+        __m128d cvec2 = _mm_loadu_pd(&C[i+j*lda+4]);
+        __m128d avec2 = _mm_loadu_pd(&A[i+k*lda+4]);
+        __m128d cvec3 = _mm_loadu_pd(&C[i+j*lda+6]);
+        __m128d avec3 = _mm_loadu_pd(&A[i+k*lda+6]);
+
+        cvec = _mm_add_pd(cvec, _mm_mul_pd(avec,bvec));
+        cvec1 = _mm_add_pd(cvec1, _mm_mul_pd(avec1,bvec));
+        cvec2 = _mm_add_pd(cvec2, _mm_mul_pd(avec2,bvec));
+        cvec3 = _mm_add_pd(cvec3, _mm_mul_pd(avec3,bvec));
+
+        _mm_storeu_pd(&C[i+j*lda],cvec);
+        _mm_storeu_pd(&C[i+j*lda+2],cvec1);
+        _mm_storeu_pd(&C[i+j*lda+4],cvec2);
+        _mm_storeu_pd(&C[i+j*lda+6],cvec3);
+      }
+    for (int i=M/8*8; i< M; i ++) 
+      {
+        double cij = C[i+j*lda];
+        cij += A[i+k*lda] * B[k+j*lda];
+        C[i+j*lda] = cij;
+      }
     }
+  }
 }
 
 /* This routine performs a dgemm operation
  *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in column-major format. 
  * On exit, A and B maintain their input values. */  
-void square_dgemm (int lda, double* A, double* B, double* C)
+void square_dgemm (int lda, double* A, double* B, double *restrict C)
 {
-  /* For each block-row of A */ 
-  for (int j = 0; j < lda; j += BLOCK_SIZE)
-    /* For each block-column of B */
-    for (int k = 0; k < lda; k += BLOCK_SIZE)
-      /* Accumulate block dgemms into block of C */
-      for (int i = 0; i < lda; i += BLOCK_SIZE)
+  /* For each block-column of B */
+    for (int j = 0; j < lda; j += BLOCK_SIZE)
       {
-	/* Correct block dimensions if block "goes off edge of" the matrix */
-	int M = min (BLOCK_SIZE, lda-i);
-	int N = min (BLOCK_SIZE, lda-j);
-	int K = min (BLOCK_SIZE, lda-k);
-
-	/* Perform individual block dgemm */
-	do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
+      int N = min (BLOCK_SIZE, lda-j);
+      /* Accumulate block dgemms into block of C */
+      for (int k = 0; k < lda; k += BLOCK_SIZE)
+        {
+          int K = min (BLOCK_SIZE, lda-k);
+        /* For each block-row of A */ 
+        for (int i = 0; i < lda; i += BLOCK_SIZE)
+          {
+          /* Correct block dimensions if block "goes off edge of" the matrix */
+          int M = min (BLOCK_SIZE, lda-i);
+          /* Perform individual block dgemm */
+          do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
       }
+    }
+  }
 }
--- dgemm-naive.c
+++ dgemm-naive.c
@@ -1,3 +1,5 @@
+#include <emmintrin.h>
+
 const char* dgemm_desc = "Naive, three-loop dgemm.";
 
 /* This routine performs a dgemm operation
@@ -6,15 +8,40 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
  * On exit, A and B maintain their input values. */    
 void square_dgemm (int n, double* A, double* B, double* C)
 {
-  /* For each row i of A */
-  for (int j = 0; j < n; ++j)
-    /* For each column j of B */
-    for (int k = 0; k < n; ++k) 
+  /* For each column j of B */
+  for (int j = 0; j < n; ++j) 
+  {
+    for( int k = 0; k < n; k++ )
     {
-      /* Compute C(i,j) */
-      //double cij = C[i+j*n];
-      double bkj = B[k+j*n];
-      for( int i = 0; i < n; i++ )
-	C[i+j*n] += A[i+k*n] * bkj;
+      __m128d bvec = _mm_load1_pd(&B[k+j*n]);
+      /* For each row i of A */
+      for (int i = 0; i < n/8*8; i += 8)
+      {
+        /* Compute C(i,j) */
+        __m128d cvec = _mm_loadu_pd(&C[i+j*n]);
+        __m128d avec = _mm_loadu_pd(&A[i+k*n]);
+        cvec = _mm_add_pd(cvec, _mm_mul_pd(avec,bvec));
+        _mm_storeu_pd(&C[i+j*n],cvec);
+        cvec = _mm_loadu_pd(&C[i+j*n+2]);
+        avec = _mm_loadu_pd(&A[i+k*n+2]);
+        cvec = _mm_add_pd(cvec, _mm_mul_pd(avec,bvec));
+        _mm_storeu_pd(&C[i+j*n+2],cvec);
+        cvec = _mm_loadu_pd(&C[i+j*n+4]);
+        avec = _mm_loadu_pd(&A[i+k*n+4]);
+        cvec = _mm_add_pd(cvec, _mm_mul_pd(avec,bvec));
+        _mm_storeu_pd(&C[i+j*n+4],cvec);
+        cvec = _mm_loadu_pd(&C[i+j*n+6]);
+        avec = _mm_loadu_pd(&A[i+k*n+6]);
+        cvec = _mm_add_pd(cvec, _mm_mul_pd(avec,bvec));
+        _mm_storeu_pd(&C[i+j*n+6],cvec);
+
+      }
+      for (int i = n/8*8; i < n; i ++) 
+      {
+        double cij = C[i+j*n];
+        cij += A[i+k*n] * B[k+j*n];
+        C[i+j*n] = cij;
+      }
     }
+  }
 }
