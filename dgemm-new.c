c15cd30624397e16d056d0c80f6dd84eef7a4bbe
Julie Newcomb <jn80842@gmail.com>
2015-02-11 23:47:49 -0800

    working with tutorial

--- dgemm-new.c
+++ dgemm-new.c
@@ -1,61 +1,34 @@
-const char* dgemm_desc = "Simple new dgemm.";
+#include <emmintrin.h>
 
-#if !defined(BLOCK_SIZE)
-#define BLOCK_SIZE 72
-#endif
+const char* dgemm_desc = "tutorial based dgemm.";
 
-#define min(a,b) (((a)<(b))?(a):(b))
-
-/* This auxiliary subroutine performs a smaller dgemm operation
- *  C := C + A * B
- * where C is M-by-N, A is M-by-K, and B is K-by-N. */
-static void do_block (int lda, int M, int N, int K, double* A, double* B, double* C)
+void add_dot(int lda, int stride, double* A, double* B, double* C)
 {
-  for (int j = 0; j < N; ++j)
+  for (int k=0; k<lda;k++)
   {
-    int step1 = 4;
-    for (int k = 0; k < K; k += step1) 
-    {
-      step1 = min ( K - k, step1 );
-      double bkj = 0, bk1j = 0, bk2j = 0, bk3j = 0;
-      switch (step1) {
-        case 4:
-          bk3j = B[k + 3 + j * lda];
-        case 3:
-          bk2j = B[k + 2 + j * lda];
-        case 2:
-          bk1j = B[k + 1 + j * lda];
-        case 1:
-          bkj = B[k + j * lda];
-          break;
-      }
-
-      for (int i = 0; i < M; ++i) {
-      	C[i+j*lda] += A[i+k*lda] * bkj;
-        C[i+j*lda] += A[i+(k + 1)*lda] * bk1j;
-        C[i+j*lda] += A[i+(k + 2)*lda] * bk2j;
-        C[i+j*lda] += A[i+(k + 3)*lda] * bk3j;
-      }
-    }
+    *C += A[k*stride] * B[k];
+            /* Compute C(i,j) */
+        // double cij = C[i+j*n];
+        // cij += A[i+k*n] * B[k+j*n];
+        // C[i+j*n] = cij;
   }
 }
 
+
 /* This routine performs a dgemm operation
  *  C := C + A * B
- * where A, B, and C are lda-by-lda matrices stored in column-major format. 
- * On exit, A and B maintain their input values. */  
+ * where A, B, and C are lda-by-lda matrices stored in column-major format.
+ * On exit, A and B maintain their input values. */    
 void square_dgemm (int lda, double* A, double* B, double* C)
 {
-  for (int j = 0; j < lda; j += BLOCK_SIZE)
-    for (int k = 0; k < lda; k += BLOCK_SIZE)
-      for (int i = 0; i < lda; i += BLOCK_SIZE)
-      {
-      	int M = min (BLOCK_SIZE, lda-i);
-	      int N = min (BLOCK_SIZE, lda-j);
-	      int K = min (BLOCK_SIZE, lda-k);
-
-	      /* Perform individual block dgemm */
-	      do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
-      }
+  /* For each column j of B */
+  for (int j = 0; j < lda; ++j) // columns of C
+  {
+    /* For each row i of A */
+    for (int i = 0; i < lda; i ++) // rows of C
+    {
+      //AddDot( k, &A( i,0 ), lda, &B( 0,j ), &C( i,j ) );
+      add_dot(lda,lda,A+i,B+j*lda,C+i+j*lda);
+    }
+  }
 }
-
