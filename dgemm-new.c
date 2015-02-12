dc01972d379939904f32899c0981e2c8a44977db
Julie Newcomb <jn80842@gmail.com>
2015-02-12 00:01:46 -0800

    tutorial step 3

--- dgemm-new.c
+++ dgemm-new.c
@@ -7,13 +7,16 @@ void add_dot(int lda, int stride, double* A, double* B, double* C)
   for (int k=0; k<lda;k++)
   {
     *C += A[k*stride] * B[k];
-            /* Compute C(i,j) */
-        // double cij = C[i+j*n];
-        // cij += A[i+k*n] * B[k+j*n];
-        // C[i+j*n] = cij;
   }
 }
 
+void add_dot_1x4(int lda, double* A, double* B, double* C) 
+{
+  add_dot(lda,lda,A,B,C);
+  add_dot(lda,lda,A,B+lda,C+lda);
+  add_dot(lda,lda,A,B+2*lda,C+2*lda);
+  add_dot(lda,lda,A,B+3*lda,C+2*lda);
+}
 
 /* This routine performs a dgemm operation
  *  C := C + A * B
@@ -22,13 +25,13 @@ void add_dot(int lda, int stride, double* A, double* B, double* C)
 void square_dgemm (int lda, double* A, double* B, double* C)
 {
   /* For each column j of B */
-  for (int j = 0; j < lda; ++j) // columns of C
+  for (int j = 0; j < lda; j+=4) // columns of C
   {
     /* For each row i of A */
     for (int i = 0; i < lda; i ++) // rows of C
     {
       //AddDot( k, &A( i,0 ), lda, &B( 0,j ), &C( i,j ) );
-      add_dot(lda,lda,A+i,B+j*lda,C+i+j*lda);
+      add_dot_1x4(lda, A+i, B+(j*lda),C+i+(j*lda));
     }
   }
 }
